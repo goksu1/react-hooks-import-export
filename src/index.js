0d5709f0ed48041e2a5ef90826654ec4e4f3ba7e
goksu1 <goksu.baykal@gmail.com>
2022-03-03 14:16:31 +0300

    Done.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import ColoradoStateParks from "./ColoradoStateParks";
+import HowManyParks from "../src/parks/howManyParks";
+import MesaVerde from "../src/parks/MesaVerde";
+import RockyMountain from "../src/parks/RockyMountain";
 
-ReactDOM.render(<ColoradoStateParks />, document.getElementById("root"));
+
+ReactDOM.render(<ColoradoStateParks />,
+    
+    <HowManyParks/>,
+    <MesaVerde/>,
+    <RockyMountain/>,
+    
+    document.getElementById("root"));
